b94f1763aed70bef65fae8752cec09d36da20ef9
Robin Lambertz <robinpub@googlemail.com>
2012-01-29 00:05:29 +0000

    Finished static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,7 @@ describe "Static pages" do
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
-															 :title => '| Home')
+															 :text => '| Home')
 		end
 	end
 
